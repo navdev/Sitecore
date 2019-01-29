e1bbed97ed5b38fb4e3a52d66e208728abcc8d41
Navneet Gupta <n.gupta@proyecta.local>
2019-01-29 12:46:18 +0000

    created project for testing lightinject

---
+++ src/Democore/Democore.Project.Website/DependencyInjection/DefaultServiceProviderBuilder.cs
@@ -0,0 +1,26 @@
+﻿using LightInject;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LightInject.Microsoft.DependencyInjection;
+using Sitecore.Abstractions;
+
+namespace Democore.Project.Website.DependencyInjection
+{
+    public class DefaultServiceProviderBuilder : BaseServiceProviderBuilder
+    {
+        protected override IServiceProvider BuildServiceProvider(IServiceCollection serviceCollection)
+        {
+            System.Diagnostics.Debugger.Launch();
+            var container = new ServiceContainer() { PropertyDependencySelector = new PropertyInjectionDisabler() };
+            var lightInjectProvider = container.CreateServiceProvider(serviceCollection);
+            var microsoftProvider = serviceCollection.BuildServiceProvider();
+            var t = lightInjectProvider.GetService<BaseCorePipelineManager>();
+            var t1 = microsoftProvider.GetService<BaseCorePipelineManager>();
+            return lightInjectProvider;
+        }
+    }
+}
---
+++ src/Democore/Democore.Project.Website/DependencyInjection/PropertyInjectionDisabler.cs
@@ -0,0 +1,16 @@
+﻿using LightInject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Democore.Project.Website.DependencyInjection
+{
+    public class PropertyInjectionDisabler : IPropertyDependencySelector
+    {
+        public IEnumerable<PropertyDependency> Execute(Type type)
+        {
+            return new PropertyDependency[0];
+        }
+    }
+}
